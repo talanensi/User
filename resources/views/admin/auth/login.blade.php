9ef18cd88cfb94d80a824cf998357eeb5159bd6b
talanensi <nensitala2001@gmail.com>
2021-08-28 15:28:46 +0530

    change

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -58,15 +58,6 @@
                                                     </div>
                                                 </div>
                 
-                                                <div class="form-group row">
-                                                    <div class="col-12">
-                                                        <div class="custom-control custom-checkbox">
-                                                            <input type="checkbox" class="custom-control-input" name="remember" id="customCheck1">
-                                                            <label class="custom-control-label" for="customCheck1">Remember me</label>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                
                                                 <div class="form-group text-center row m-t-20">
                                                     <div class="col-12">
                                                         <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">Log In</button>
@@ -85,5 +76,47 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/js/modernizr.min.js') }}"></script>
+        <script src="{{ asset('assets/js/detect.js') }}"></script>
+        <script src="{{ asset('assets/js/fastclick.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.slimscroll.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.blockUI.js') }}"></script>
+        <script src="{{ asset('assets/js/waves.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.nicescroll.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery.scrollTo.min.js') }}"></script>
+
+        <script src="{{ asset('assets/js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+
+        <script>
+            $("#admin_login").validate({
+                errorClass: 'invalid-feedback animated fadeInDown',
+                errorElement: 'div',
+                rules: {
+                    email: {
+                        required: true,
+                    },
+                    password: {
+                        required: true,
+                    },
+                },
+                messages: {
+                    email: {
+                        required: "The email field is required.",
+                    },
+                    password: {
+                        required: "The password field is required.",
+                    },
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).parents("div.form-control").addClass(errorClass).removeClass(validClass);
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).parents(".error").removeClass(errorClass).addClass(validClass);
+                }
+            });
+        </script>
 </body>
 </html>
